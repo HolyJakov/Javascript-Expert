e4d6403ff71ded709ab59baf94fd02109e85b18d
Holy Ojakovo <ojakovoofuafo@gmail.com>
2023-01-27 13:22:25 +0100

    javascript

---
+++ Javascriptfirst/app.js
@@ -0,0 +1,33 @@
+const string = "the quick brown fox";
+function capitalFirstLetter(hol){
+    const strArry = hol.toLowerCase().split(' ');
+    
+    for(let i=0; i<strArry.length; i++) {
+        strArry[i] = strArry[i].charAt(0).toUpperCase() + strArry[i].slice(1);
+    }
+    const result = strArry.join(' ');
+    return result;
+}
+
+const res = capitalFirstLetter(string);
+console.log(res)
+
+
+
+
+function evenOrNot(number){
+    if(number % 2 == 0) {
+        console.log("even number");
+    }
+    else{
+        console.log('odd number');
+    }
+}
+const answer =evenOrNot(4);
+
+
+
+function arrayPar(arry){
+console.log(arry);
+}
+arrayPar([1,2,3]);
